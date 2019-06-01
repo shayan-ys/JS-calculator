3f1ad67271aecd81a44e319769a7d3292e47ef56
Shayan Ys <shayan.yousefian1372@gmail.com>
2019-06-01 14:27:48 -0400

    Add link to app button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Grid } from '@material-ui/core';
+import {Grid, makeStyles} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Hidden from '@material-ui/core/Hidden';
 
 import { CalculatorBox } from './containers/CalculatorBox';
 
+const useStyles = makeStyles(theme => ({
+    appsLink: {
+        position: "absolute",
+        left: 25
+    },
+}));
+
 const App: React.FC = () => {
+    const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
+          <Button href="/" variant="contained" color="primary" className={classes.appsLink}>
+              &#65308; <Hidden xsDown>Back to Apps</Hidden>
+          </Button>
          <Grid
               container
               direction="row"
@@ -22,7 +35,7 @@ const App: React.FC = () => {
             <img src={logo} className="App-logo" alt="logo" />
           </Grid>
           <Grid item className="App-header-container">
-              <b><a href="http://shayanys.com" style={{color: "white", textDecoration: "none"}}>ShayanYs</a></b>
+              <b><a href="http://app.shayanys.com" style={{color: "white", textDecoration: "none"}}>ShayanYs</a></b>
           </Grid>
         </Grid>
       </header>
